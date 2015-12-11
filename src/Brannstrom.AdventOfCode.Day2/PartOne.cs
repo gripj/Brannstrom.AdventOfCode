c5375c4901acb9a8c0de4feef221ec6486178c60
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2015-12-11 20:41:29 +0100

    Calculate wrapping paper area for a present box

---
+++ src/Brannstrom.AdventOfCode.Day2/PartOne.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day2
+{
+    [TestFixture]
+    public class PartOne
+    {
+        [Test]
+        [TestCase(2,3,4,58)]
+        [TestCase(1,1,10,43)]
+        public void Should_Calculate_Wrapping_Paper_Area(int l, int w, int h, int expectedWrappingPaperArea)
+        {
+            new PresentBox(l, w, h).WrappingPaperSize.Should().Be(expectedWrappingPaperArea);
+        }
+    }
+}
---
+++ src/Brannstrom.AdventOfCode.Day2/PresentBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Brannstrom.AdventOfCode.Day2
+{
+    public class PresentBox
+    {
+        public int Length { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public PresentBox(int length, int width, int height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public int WrappingPaperSize => SurfaceArea + SmallestSide;
+
+        private int SurfaceArea => 2 * Length * Width + 2 * Width * Height + 2 * Height * Length;
+
+        private int SmallestSide => new List<int>() { Length * Width, Width * Height, Height * Length }.Min();
+    }
+}
