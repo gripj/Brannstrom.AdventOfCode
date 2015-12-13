76440423a6b033114d75db111a16ba9c9ce374a2
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2015-12-13 10:25:43 +0100

    Complete Day 8

---
+++ src/Brannstrom.AdventOfCode.Day8/PartOne.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day8
+{
+    [TestFixture]
+    public class PartOne
+    {
+        [Test]
+        public void Should_Count_Difference()
+        {
+            Count_Difference().Should().Be(1350);
+        }
+
+        private int Count_Difference()
+        {
+            var fileStrings = new Reader().ReadStringsFromFile();
+            return fileStrings.Sum(c => c.Length - (Regex.Unescape(c).Length - 2));
+        }
+    }
+}
---
+++ src/Brannstrom.AdventOfCode.Day8/PartTwo.cs
@@ -0,0 +1,35 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day8
+{
+    [TestFixture]
+    public class PartTwo
+    {
+        [TestCase]
+        public void Should_Count_Difference()
+        {
+            CountDifference().Should().Be(2085);
+        }
+
+        private static int CountDifference()
+        {
+            var fileStrings = new Reader().ReadStringsFromFile();
+            return fileStrings.Sum(c => ToLiteral(c).Length - c.Length);
+        }
+
+        private static string ToLiteral(string input)
+        {
+            using (var writer = new StringWriter())
+                using (var provider = CodeDomProvider.CreateProvider("CSharp"))
+                {
+                    provider.GenerateCodeFromExpression(new CodePrimitiveExpression(input), writer, null);
+                    return writer.ToString();
+                }
+        }
+    }
+}
