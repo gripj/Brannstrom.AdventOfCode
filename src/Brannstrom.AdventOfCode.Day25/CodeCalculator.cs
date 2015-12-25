9a0e27135dbe534c40ac3edffe8d81ea05528638
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2015-12-25 09:32:23 +0100

    Complete Day 25 and Advent of Code 2015

---
+++ src/Brannstrom.AdventOfCode.Day25/CodeCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace Brannstrom.AdventOfCode.Day25
+{
+    public class CodeCalculator
+    {
+        public long Calculate(int row, int column)
+        {
+            var code = 20151125L;
+            var gridPosition = GetPositionInGrid(row, column);
+
+            for (var i = 1; i < gridPosition; i++)
+            {
+                code *= 252533;
+                code %= 33554393;
+            }
+
+            return code;
+        }
+
+        private static int GetPositionInGrid(int row, int column)
+        {
+            var position = row + column - 2;
+            position = position * (position + 1) / 2 + column;
+            return position;
+        }
+    }
+}
---
+++ src/Brannstrom.AdventOfCode.Day25/PartOne.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day25
+{
+    [TestFixture]
+    public class PartOne
+    {
+        [Test]
+        public void Should_Calculate_Code()
+        {
+            const int row = 2947;
+            const int column = 3029;
+            new CodeCalculator().Calculate(row, column).Should().Be(19980801);
+        }
+
+        [Test]
+        [TestCase(2, 1, 31916031)]
+        [TestCase(3, 4, 7981243)]
+        [TestCase(6, 5, 1534922)]
+        public void Should_Calculate_Codes_Correctly(int row, int column, int expectedValue)
+        {
+            new CodeCalculator().Calculate(row, column).Should().Be(expectedValue);
+        }
+    }
+}
