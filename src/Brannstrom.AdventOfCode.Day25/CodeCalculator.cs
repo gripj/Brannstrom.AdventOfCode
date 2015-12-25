bf99375582f3ebd78c63ecdd1110472b64d13bd9
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2015-12-25 17:34:46 +0100

    Clean Day 25

--- src/Brannstrom.AdventOfCode.Day25/CodeCalculator.cs
+++ src/Brannstrom.AdventOfCode.Day25/CodeCalculator.cs
@@ -2,25 +2,33 @@
 {
     public class CodeCalculator
     {
+        private const long FirstCode = 20151125L;
+        public readonly int Multiplier = 252533;
+        public readonly int Divider = 33554393;
+
         public long Calculate(int row, int column)
         {
-            var code = 20151125L;
-            var gridPosition = GetPositionInGrid(row, column);
+            var code = FirstCode;
+            var codeNumber = GetCodeNumber(row, column);
 
-            for (var i = 1; i < gridPosition; i++)
-            {
-                code *= 252533;
-                code %= 33554393;
-            }
+            for (var i = 1; i < codeNumber; i++)
+                code = CalculateNextCode(code);
 
             return code;
         }
 
-        private static int GetPositionInGrid(int row, int column)
+        private static int GetCodeNumber(int row, int column)
         {
             var position = row + column - 2;
             position = position * (position + 1) / 2 + column;
             return position;
+        }
+
+        private long CalculateNextCode(long code)
+        {
+            code *= Multiplier;
+            code %= Divider;
+            return code;
         }
     }
 }
--- src/Brannstrom.AdventOfCode.Day25/PartOne.cs
+++ src/Brannstrom.AdventOfCode.Day25/PartOne.cs
@@ -6,12 +6,20 @@ namespace Brannstrom.AdventOfCode.Day25
     [TestFixture]
     public class PartOne
     {
+        private CodeCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new CodeCalculator();
+        }
+
         [Test]
         public void Should_Calculate_Code()
         {
             const int row = 2947;
             const int column = 3029;
-            new CodeCalculator().Calculate(row, column).Should().Be(19980801);
+            _calculator.Calculate(row, column).Should().Be(19980801);
         }
 
         [Test]
@@ -20,7 +28,7 @@ namespace Brannstrom.AdventOfCode.Day25
         [TestCase(6, 5, 1534922)]
         public void Should_Calculate_Codes_Correctly(int row, int column, int expectedValue)
         {
-            new CodeCalculator().Calculate(row, column).Should().Be(expectedValue);
+            _calculator.Calculate(row, column).Should().Be(expectedValue);
         }
     }
 }
