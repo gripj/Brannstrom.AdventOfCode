0fd50b8760891f941032b329d4a9d7ba6d1e24c6
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2016-12-23 11:28:32 +0100

    Complete Day 22

---
+++ src/2016/Brannstrom.AdventOfCode.Day22/GridComputer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Brannstrom.AdventOfCode.Day22
+{
+    public class GridComputer
+    {
+        public long CountViablePairsOfNodes(IEnumerable<Node> nodes)
+        {
+            return GetAllPermutations(nodes)
+                        .ToList()
+                        .FindAll(n => (n.Item1.Used > 0 && n.Item2.Avail >= n.Item1.Used) || 
+                                        (n.Item2.Used > 0 && n.Item1.Avail >= n.Item2.Used))
+                        .Count;
+        }
+
+        public int CalculateFewestNumberOfStepsToMoveGoalData(IEnumerable<Node> nodes, int size)
+        {
+            var goal = nodes
+                        .Where(n => n.Y == 0)
+                        .Aggregate((first, second) => first.X > second.X ? first : second);
+
+            var emptyNode = nodes.Aggregate((first, second) => first.Avail > second.Avail ? first : second);
+            var edgeNodes = nodes.Where(n => n.Used > emptyNode.Avail).ToList();
+            var edgeNodeWithSmallestXCoordinate = edgeNodes.Aggregate((first, second) => first.X < second.X ? first : second);
+
+            return (goal.X - 1) * size +
+                          emptyNode.Y +
+                          edgeNodes.Count + (emptyNode.X - edgeNodeWithSmallestXCoordinate.X) + 1;
+        }
+
+        private static IEnumerable<Tuple<T, T>> GetAllPermutations<T>(IEnumerable<T> list)
+        {
+            var combinations = from pair in list.Select((value, index) => new { value, index })
+                               from second in list.Skip(pair.index + 1)
+                               select Tuple.Create(pair.value, second);
+
+            return combinations;
+        }
+    }
+}
---
+++ src/2016/Brannstrom.AdventOfCode.Day22/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Brannstrom.AdventOfCode.Day22
+{
+    public class Node
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Size { get; }
+        public int Used { get; }
+        public int Avail { get; }
+        public int UsePercent { get; }
+
+        public Node(
+            int x,
+            int y,
+            int size,
+            int used,
+            int avail,
+            int usePercent)
+        {
+            X = x;
+            Y = y;
+            Size = size;
+            Used = used;
+            Avail = avail;
+            UsePercent = usePercent;
+        }
+
+        public static Node Parse(string input)
+        {
+            var parts = input.Split(new [] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var x = int.Parse(parts[0].Split('x')[1].Split('-')[0]);
+            var y = int.Parse(parts[0].Split('y')[1]);
+            var size = int.Parse(parts[1].Substring(0, parts[1].Length - 1));
+            var used = int.Parse(parts[2].Substring(0, parts[2].Length - 1));
+            var avail = int.Parse(parts[3].Substring(0, parts[3].Length - 1));
+            var usePercent = int.Parse(parts[4].Substring(0, parts[4].Length - 1));
+
+            return new Node(x, y, size, used, avail, usePercent);
+        }
+    }
+}
