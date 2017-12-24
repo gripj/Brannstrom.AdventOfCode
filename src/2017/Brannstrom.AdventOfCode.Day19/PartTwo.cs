99432521790c5496fa2565aaf854f8b940510f63
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2017-12-24 21:37:57 +0100

    Complete day 19

---
+++ src/2017/Brannstrom.AdventOfCode.Day19/PartTwo.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day19
+{
+    [TestFixture]
+    public class PartTwo
+    {
+        [Test]
+        public void Should_Count_Number_Of_Steps_In_Example()
+        {
+            new PathSolver()
+                .FollowPath(File.ReadAllLines("exampleInput.txt"))
+                .Steps
+                .Should()
+                .Be(38);
+        }
+
+        [Test]
+        public void Should_Count_Number_Of_Steps()
+        {
+            new PathSolver()
+                .FollowPath(File.ReadAllLines("input.txt"))
+                .Steps
+                .Should()
+                .Be(17358);
+        }
+    }
+}
---
+++ src/2017/Brannstrom.AdventOfCode.Day19/PathSolver.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Brannstrom.AdventOfCode.Day19
+{
+    public class PathSolver
+    {
+        public (string Letters, int Steps) FollowPath(IEnumerable<string> input)
+        {
+            var map = input.ToList();
+            var (columnSize, rowSize) = (map[0].Length, map.Count);
+            var (column, row) = (map[0].IndexOf('|'), 0);
+            var (deltaColumn, deltaRow) = (0, 1);
+
+            var letters = "";
+            var steps = 0;
+
+            while (true)
+            {
+                row += deltaRow;
+                column += deltaColumn;
+                steps++;
+
+                var hasReachedEnd = column < 0 || column >= columnSize || row < 0 || row >= rowSize || map[row][column] == ' ';
+                if (hasReachedEnd)
+                    break;
+
+                switch (map[row][column])
+                {
+                    case '+':
+                        (deltaColumn, deltaRow) = (
+                            from q in new[] { (deltaRow: deltaColumn, deltaColumn: -deltaRow), (deltaRow: -deltaColumn, deltaColumn: deltaRow) }
+                            let colT = column + q.deltaColumn
+                            let rowT = row + q.deltaRow
+                            where colT >= 0 && colT < columnSize && rowT >= 0 && rowT < rowSize && map[rowT][colT] != ' '
+                            select (q.deltaColumn, q.deltaRow)
+                        ).Single();
+                        break;
+                    case char @char when (@char >= 'A' && @char <= 'Z'):
+                        letters += @char;
+                        break;
+                }
+            }
+
+            return (letters, steps);
+        }
+    }
+}
