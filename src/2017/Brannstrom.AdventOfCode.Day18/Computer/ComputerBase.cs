3b5a14bf49d9fb9fab2d294dd48f4e8ef66a398d
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2017-12-24 20:21:21 +0100

    Complete day 18

---
+++ src/2017/Brannstrom.AdventOfCode.Day18/Computer/ComputerBase.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Brannstrom.AdventOfCode.Day18.Computer
+{
+    public abstract class ComputerBase<TState>
+    {
+        private readonly Dictionary<string, long> _registers = new Dictionary<string, long>();
+
+        protected bool IsExecuting;
+        protected int InstructionPosition;
+
+        protected long this[string register]
+        {
+            get
+            {
+                return long.TryParse(register, out var n) ? n
+                    : _registers.ContainsKey(register) ? _registers[register]
+                    : 0;
+            }
+            set
+            {
+                _registers[register] = value;
+            }
+        }
+
+        public IEnumerable<TState> Execute(IEnumerable<string> input)
+        {
+            var instructions = input.ToArray();
+
+            while (InstructionPosition >= 0 && InstructionPosition < instructions.Length)
+            {
+                IsExecuting = true;
+                var parts = instructions[InstructionPosition].Split(' ');
+                switch (parts[0])
+                {
+                    case "snd": Snd(parts[1]); break;
+                    case "rcv": Rcv(parts[1]); break;
+                    case "set": Set(parts[1], parts[2]); break;
+                    case "add": Add(parts[1], parts[2]); break;
+                    case "mul": Mul(parts[1], parts[2]); break;
+                    case "mod": Mod(parts[1], parts[2]); break;
+                    case "jgz": Jgz(parts[1], parts[2]); break;
+                }
+                yield return State();
+            }
+
+            IsExecuting = false;
+            yield return State();
+        }
+
+        protected void Set(string registerA, string registerB)
+        {
+            this[registerA] = this[registerB];
+            InstructionPosition++;
+        }
+
+        protected void Add(string registerA, string registerB)
+        {
+            this[registerA] += this[registerB];
+            InstructionPosition++;
+        }
+
+        protected void Mul(string registerA, string registerB)
+        {
+            this[registerA] *= this[registerB];
+            InstructionPosition++;
+        }
+
+        protected void Mod(string registerA, string registerB)
+        {
+            this[registerA] %= this[registerB];
+            InstructionPosition++;
+        }
+
+        protected void Jgz(string registerA, string registerB)
+        {
+            InstructionPosition += this[registerA] > 0 ? (int)this[registerB] : 1;
+        }
+
+        protected abstract TState State();
+
+        protected abstract void Snd(string reg);
+
+        protected abstract void Rcv(string reg);
+    }
+}
---
+++ src/2017/Brannstrom.AdventOfCode.Day18/Computer/ComputerV2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Brannstrom.AdventOfCode.Day18.Computer
+{
+    public class ComputerV2 : ComputerBase<(bool IsActive, int TimesSent)>
+    {
+        private int _timesSent;
+
+        private readonly Queue<long> _inQueue;
+        private readonly Queue<long> _outQueue;
+
+        public ComputerV2(long p, Queue<long> inQueue, Queue<long> outQueue)
+        {
+            this["p"] = p;
+            _inQueue = inQueue;
+            _outQueue = outQueue;
+        }
+
+        protected override (bool IsActive, int TimesSent) State()
+        {
+            return (IsActive: IsExecuting, TimesSent: _timesSent);
+        }
+
+        protected override void Snd(string reg)
+        {
+            _outQueue.Enqueue(this[reg]);
+            _timesSent++;
+            InstructionPosition++;
+        }
+
+        protected override void Rcv(string reg)
+        {
+            if (_inQueue.Any())
+            {
+                this[reg] = _inQueue.Dequeue();
+                InstructionPosition++;
+            }
+            else
+                IsExecuting = false;
+        }
+    }
+}
