8f59512b8ace8b5609ef15362cc78660e69ad92f
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2017-12-25 22:15:05 +0100

    Complete day 24

---
+++ src/2017/Brannstrom.AdventOfCode.Day24/Component.cs
@@ -0,0 +1,14 @@
+﻿namespace Brannstrom.AdventOfCode.Day24
+{
+    public class Component
+    {
+        public int PortA { get; }
+        public int PortB { get; }
+
+        public Component(int portA, int portB)
+        {
+            PortA = portA;
+            PortB = portB;
+        }
+    }
+}
---
+++ src/2017/Brannstrom.AdventOfCode.Day24/PartTwo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brannstrom.AdventOfCode.Day24
+{
+    [TestFixture]
+    public class PartTwo
+    {
+        [Test]
+        public void Should_Get_Strength_Of_Longest_Bridge_In_Example()
+        {
+            var components = new List<string>()
+            {
+                "0/2",
+                "2/2",
+                "2/3",
+                "3/4",
+                "3/5",
+                "0/1",
+                "10/1",
+                "9/10"
+            };
+
+            new BridgeBuilder().GetStrengthOfLongestBridge(components).Should().Be(19);
+        }
+
+        [Test]
+        public void Should_Get_Strength_Of_Longest_Bridge()
+        {
+            new BridgeBuilder().GetStrengthOfLongestBridge(File.ReadAllLines("input.txt")).Should().Be(1841);
+        }
+    }
+}
